62706cc0d1aa5b8dba3042f70938a9d4adbe03c2
Ben <ben.larson@hotmail.com>
2024-07-12 14:12:56 +0000

    Add a failing test that reproduces the multi-threaded problem :)

--- tests/rate_limiter.rs
+++ tests/rate_limiter.rs
@@ -78,10 +78,67 @@ async fn test_multiple_concurrent() {
     assert_eq!(Instant::now() - start, Duration::from_secs(2)); // We must have waited 2 seconds to fire 3 things
 }
 
-/// Have 5 threads, each submitting 3 jobs. All jobs will have weight 1. We will use barriers such that
+/// Have 5 tasks, each submitting 3 jobs. All jobs will have weight 1. We will use barriers such that
 /// the jobs are submitted in batches; one from each thread. We want to check that the number of jobs executed
 /// each second is as expected.
 #[tokio::test(start_paused = true)]
+async fn test_multi_tasked() {
+    let rate_limiter = Arc::new(RateLimiter::new(3, Duration::from_secs(1)));
+    let num_threads = 5;
+    let num_batches = 3;
+
+    let barriers = Arc::new(vec![
+        Barrier::new(num_threads),
+        Barrier::new(num_threads),
+        Barrier::new(num_threads),
+    ]);
+
+    let second_to_result_count = Arc::new(Mutex::new(HashMap::new()));
+
+    let start = Instant::now();
+
+    let mut futures = Vec::new();
+
+    // Queue up all of the tasks immediately, but in the execution order defined by job_ids
+    for _ in 0..num_threads {
+        let rate_limiter = Arc::clone(&rate_limiter);
+        let barriers = Arc::clone(&barriers);
+        let second_to_result_count = Arc::clone(&second_to_result_count);
+
+        let fut = task::spawn(async move {
+            for batch in 0..num_batches {
+                // Just record when this is evaluated
+                let fut = async {
+                    let duration: u64 = (Instant::now() - start).as_secs();
+                    second_to_result_count
+                        .lock()
+                        .await
+                        .entry(duration)
+                        .and_modify(|x| *x += 1)
+                        .or_insert(1);
+                };
+
+                // Wait until all threads are ready to submit their jobs.
+                barriers[batch].wait().await;
+                rate_limiter.rate_limit_future(fut, 1).await;
+            }
+        });
+
+        futures.push(fut);
+    }
+
+    // Just make sure they're all finished.
+    join_all(futures).await;
+
+    let expected = HashMap::from([(0, 3), (1, 3), (2, 3), (3, 3), (4, 3)]);
+
+    assert_eq!(*second_to_result_count.lock().await, expected);
+}
+
+/// Have 5 threads, each submitting 3 jobs. All jobs will have weight 1. We will use barriers such that
+/// the jobs are submitted in batches; one from each thread. We want to check that the number of jobs executed
+/// each second is as expected.
+#[tokio::test(flavor = "multi_thread")]
 async fn test_multi_threaded() {
     let rate_limiter = Arc::new(RateLimiter::new(3, Duration::from_secs(1)));
     let num_threads = 5;
